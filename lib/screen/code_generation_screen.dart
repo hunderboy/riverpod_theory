fd06e52e4f388f4adf08ec887b0d96dd70716de8
hunderboy <masury2011@gmail.com>
2024-04-10 19:45:01 +0900

    feat : "Code Generation"

--- lib/screen/code_generation_screen.dart
+++ lib/screen/code_generation_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_theory/layout/default_layout.dart';
+import 'package:riverpod_theory/riverpod/code_generation_provider.dart';
 import 'package:riverpod_theory/riverpod/provider.dart';
 import 'package:riverpod_theory/riverpod/state_notifier_provider.dart';
 
@@ -9,13 +10,14 @@ class CodeGenerationScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    // final filteredItems = ref.watch(filteredShoppingListProvider);
+    // code generation 을 통해 만들어진 gStateProvider(code_generation_provider.g.dart)
+    final state1 = ref.watch(gStateProvider);
 
     return DefaultLayout(
       title: 'CodeGenerationScreen',
       body: Column(
         children: [
-
+          Text('state1: $state1'),
         ],
       )
     );
